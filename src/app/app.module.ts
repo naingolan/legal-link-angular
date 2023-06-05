c5103d010a517d718e6ec2efb3d4d7ea332f6b88
naingolan <kelvinsdechaw@gmail.com>
2023-06-05 03:45:08 +0300

    another one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+
+import { MatCardModule } from '@angular/material/card';
+
+
+//These are custome components
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticateComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatListModule,
+    MatSidenavModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
